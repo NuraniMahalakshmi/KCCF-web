84227719488d0f54851d802a90b8e17be0cb019d
Logan-Ward <ward_logan@outlook.com>
2026-01-30 03:37:39 -0600

    feat: replaced "our impact" section "with how it works" section

--- src/app/crazy-socks/page.tsx
+++ src/app/crazy-socks/page.tsx
@@ -156,24 +156,97 @@ export default function CrazySocks() {
         </div>
       </section>
 
-      {/* Impact Stats */}
+      {/* How It Works */}
       <section className="py-20 bg-gradient-to-br from-platinum-50 to-platinum-100 dark:from-gray-800 dark:to-gray-700">
-        <div className="container mx-auto px-4">
-          <div className="text-center mb-16">
-            <h2 className="text-4xl md:text-5xl font-bold mb-6 text-violet-600 dark:text-white">
-              Our Impact
-            </h2>
-          </div>
-
-          <div className="grid md:grid-cols-3 gap-8">
-            {IMPACT_STATS.CRAZY_SOCKS.map((stat, index) => (
-              <div key={index} className="bg-white dark:bg-gray-800 rounded-2xl p-8 shadow-lg text-center">
-                <div className="text-4xl font-bold text-[#732154] dark:text-saffron-400 mb-4">
-                  {stat.value}
-                </div>
-                <p className="text-gray-600 dark:text-gray-200 text-lg">{stat.label}</p>
-              </div>
-            ))}
+        <div className="container text-center mx-auto px-4 sm:px-8 lg:px-10">
+          <h2 className="text-4xl md:text-5xl font-bold mb-10 text-violet-600 dark:text-white">
+            How It Works <br className="sm:hidden" /> In 3 Steps
+          </h2>
+          <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg px-4 py-10 sm:pb-13 flex-1 flex flex-col items-center">
+            <ol className="text-xl md:text-2xl text-left space-y-3 text-black dark:text-white list-none list-outside lg:grid lg:grid-cols-3 lg:gap-x-6 lg:gap-y-10 lg:space-y-0">
+              <li className="font-semibold mb-8 md:mb-10 text-center text-violet-600 dark:text-saffron-400">
+                <svg
+                  className="w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14 mx-0 inline-block"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 256 256"
+                >
+                  {/* <!-- Icon from Phosphor by Phosphor Icons - https://github.com/phosphor-icons/core/blob/main/LICENSE --> */}
+                  <path
+                    fill="currentColor"
+                    d="M252.51 108.8L227 57.75a14 14 0 0 0-18.78-6.27l-25.56 12.78l-53.13-14.06a6.1 6.1 0 0 0-3.06 0L73.34 64.26L47.79 51.48A14 14 0 0 0 29 57.75L3.49 108.8a14 14 0 0 0 6.26 18.78l27.15 13.58l55.61 39.72a6 6 0 0 0 2 .94l64 16a6 6 0 0 0 1.49.18a6 6 0 0 0 4.24-1.76l55.31-55.31l26.7-13.35a14 14 0 0 0 6.26-18.78m-53 35.16l-35.8-28.68a6 6 0 0 0-8 .45c-18.65 18.79-39.5 16.42-52.79 7.92a2 2 0 0 1-.94-1.5a1.9 1.9 0 0 1 .51-1.55L146.43 78h33.86l28.41 56.82Zm-185.4-28.27a2 2 0 0 1 .11-1.52l25.52-51.06a2 2 0 0 1 1.8-1.1a2 2 0 0 1 .89.21l22.21 11.1L37.32 128l-22.21-11.1a2 2 0 0 1-1-1.21m144.05 69.67l-59.6-14.9l-50.9-36.36l29.18-58.35L128 62.21l14.8 3.92a5.9 5.9 0 0 0-3 1.57l-45.7 44.35a14 14 0 0 0 2.39 21.72c20.22 12.92 44.75 10.49 63.8-5.89L191 152.5Zm83.73-69.67a2 2 0 0 1-1 1.16L218.68 128l-27.32-54.68l22.21-11.1a2 2 0 0 1 1.53-.11a2 2 0 0 1 1.16 1l25.52 51.06a2 2 0 0 1 .11 1.52m-112 101.76a6 6 0 0 1-7.27 4.37l-41.73-10.43a5.9 5.9 0 0 1-2-.94l-26.37-18.81a6 6 0 1 1 7-9.77L84.91 200l40.61 10.15a6 6 0 0 1 4.36 7.3Z"
+                  />
+                </svg>
+                <br className="mb-2" />
+                <span className="ml-2 text-black dark:text-white font-normal">
+                  <span className="text-violet-700 dark:text-saffron-400 font-semibold">
+                    We Coordinate
+                  </span>{' '}
+                  <br className="mb-2" /> KCCF brings all supplies to your
+                  office
+                </span>
+              </li>
+              <li className="list-item font-semibold mb-8 md:mb-10 text-center text-violet-600 dark:text-saffron-400">
+                <svg
+                  className="w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14 mx-0 inline-block"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 256 256"
+                >
+                  {/* <!-- Icon from Phosphor by Phosphor Icons - https://github.com/phosphor-icons/core/blob/main/LICENSE --> */}
+                  <path
+                    fill="currentColor"
+                    d="M196.89 130.94L144.4 111.6l-19.34-52.49a13.92 13.92 0 0 0-26.12 0L79.6 111.6l-52.49 19.34a13.92 13.92 0 0 0 0 26.12L79.6 176.4l19.34 52.49a13.92 13.92 0 0 0 26.12 0l19.34-52.49l52.49-19.34a13.92 13.92 0 0 0 0-26.12m-4.15 14.86l-55.08 20.3a6 6 0 0 0-3.56 3.56l-20.3 55.08a1.92 1.92 0 0 1-3.6 0l-20.3-55.08a6 6 0 0 0-3.56-3.56l-55.08-20.3a1.92 1.92 0 0 1 0-3.6l55.08-20.3a6 6 0 0 0 3.56-3.56l20.3-55.08a1.92 1.92 0 0 1 3.6 0l20.3 55.08a6 6 0 0 0 3.56 3.56l55.08 20.3a1.92 1.92 0 0 1 0 3.6M146 40a6 6 0 0 1 6-6h18V16a6 6 0 0 1 12 0v18h18a6 6 0 0 1 0 12h-18v18a6 6 0 0 1-12 0V46h-18a6 6 0 0 1-6-6m100 48a6 6 0 0 1-6 6h-10v10a6 6 0 0 1-12 0V94h-10a6 6 0 0 1 0-12h10V72a6 6 0 0 1 12 0v10h10a6 6 0 0 1 6 6"
+                  />
+                </svg>
+                <br className="mb-2" />
+                <span className="ml-2 text-black dark:text-white font-normal">
+                  <span className="text-violet-700 dark:text-saffron-400 font-semibold">
+                    Elana Inspires
+                  </span>{' '}
+                  <br className="mb-1" /> A 15-minute keynote that resets office
+                  culture
+                </span>
+              </li>
+              <li className="list-item font-semibold mb-9 sm:mb-11 md:mb-12 text-center text-violet-600 dark:text-saffron-400">
+                <svg
+                  className="w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14 mx-0 inline-block"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 256 256"
+                >
+                  {/* <!-- Icon from Phosphor by Phosphor Icons - https://github.com/phosphor-icons/core/blob/main/LICENSE --> */}
+                  <path
+                    fill="currentColor"
+                    d="M229.12 142.65a22.43 22.43 0 0 0-19.55-3.88l-4.32 1C227 119.55 238 99.51 238 80c0-25.36-20.39-46-45.46-46A45.51 45.51 0 0 0 156 52a45.51 45.51 0 0 0-36.54-18C94.39 34 74 54.64 74 80c0 11.38 3.63 22.49 11.29 34.36a29.73 29.73 0 0 0-16.56 8.43L45.52 146H16a14 14 0 0 0-14 14v40a14 14 0 0 0 14 14h104a6 6 0 0 0 1.46-.18l64-16a7 7 0 0 0 .89-.3L225.17 181l.33-.15a22.6 22.6 0 0 0 3.62-38.18ZM119.46 46a33.16 33.16 0 0 1 31 20.28a6 6 0 0 0 11.1 0a33.16 33.16 0 0 1 31-20.28C210.68 46 226 61.57 226 80c0 20.24-16.18 43-46.8 65.75l-14.87 3.42A26 26 0 0 0 140 114H99.67C90.36 101.67 86 90.81 86 80c0-18.43 15.32-34 33.46-34M14 200v-40a2 2 0 0 1 2-2h26v44H16a2 2 0 0 1-2-2m206.28-30l-38.2 16.27L119.26 202H54v-47.51l23.21-23.22A17.88 17.88 0 0 1 89.94 126H140a14 14 0 0 1 0 28h-28a6 6 0 0 0 0 12h32a6 6 0 0 0 1.34-.15l67-15.41l.24-.06a10.6 10.6 0 0 1 7.7 19.62"
+                  />
+                </svg>
+                <br className="mb-2" />
+                <span className="ml-2 text-black dark:text-white font-normal">
+                  <span className="text-violet-700 dark:text-saffron-400 font-semibold">
+                    Team Builds
+                  </span>{' '}
+                  <br className="mb-1" /> Employees pack bags and write notes of
+                  hope
+                </span>
+              </li>
+            </ol>
+            <a
+              href="/documents/1_24_26_KCCF.Brochure_ER.pdf"
+              download="KCCF-Crazy-Socks-Brochure"
+            >
+              <button className="cursor-pointer text-md sm:text-lg md:text-2xl btn-primary btn-primary-violet group text-white py-3 md:py-4 px-6 rounded-full font-semibold inline-flex items-center justify-center">
+                Download Our Brochure
+                <svg
+                  className="w-6 h-6 md:w-8 md:h-8 ml-2 mx-0 inline-block mt-0.5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 256 256"
+                >
+                  {/* <!-- Icon from Phosphor by Phosphor Icons - https://github.com/phosphor-icons/core/blob/main/LICENSE --> */}
+                  <path
+                    fill="currentColor"
+                    d="m213.66 82.34l-56-56A8 8 0 0 0 152 24H56a16 16 0 0 0-16 16v176a16 16 0 0 0 16 16h144a16 16 0 0 0 16-16V88a8 8 0 0 0-2.34-5.66M160 51.31L188.69 80H160ZM200 216H56V40h88v48a8 8 0 0 0 8 8h48zm-42.34-61.66a8 8 0 0 1 0 11.32l-24 24a8 8 0 0 1-11.32 0l-24-24a8 8 0 0 1 11.32-11.32L120 164.69V120a8 8 0 0 1 16 0v44.69l10.34-10.35a8 8 0 0 1 11.32 0"
+                  />
+                </svg>
+              </button>
+            </a>
           </div>
         </div>
       </section>
